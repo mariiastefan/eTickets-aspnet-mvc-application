d085bd24edf8c751ada02b8ea4e81a0a1e9af7ec
mariiastefan <mariiastefan17@gmail.com>
2023-01-13 03:40:00 +0200

    some changes

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
 
 namespace eTickets.Data
 {
     public class AppDbContext: DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) { }
+        //readonly IConfiguration _configuration;
+        //private string connectionString;
+        //public AppDbContext(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
         {
-
+            //GetData();
         }
+
+        //void GetData()
+        //{
+        //    connectionString = _configuration.GetConnectionString("DefaultConnectionString");
+        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
+
     }
 }
--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
@@ -15,6 +16,21 @@ namespace eTickets.Models
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public MovieCategory MovieCategory { get; set; }
+
+        //Relatioships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
+
+
 
     }
 }
